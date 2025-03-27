c24a2d59b482e03ab625148e09373f0b3c18e984
Justin-K-Ellis <jlke45@gmail.com>
2025-03-27 15:54:35 +0900

    Let user edit note title and content.
    My original idea was to fetch the note data with Tanstack Query, but
    it's not totally clear to me what the best way to set the state of the
    title and content is that way, so I've implemented this with useEffect
    instead for now. (I'll have to go through the Tanstack Query docs in
    more detail later.)

--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -1,27 +1,76 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import { useQuery } from "@tanstack/react-query";
-import { Note } from "../interfaces.ts";
+// import { useQuery } from "@tanstack/react-query";
+// import { Note } from "../interfaces.ts";
 
 export default function NotePage() {
+  const [editmode, setEditmode] = useState(false);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [isPending, setIsPending] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
   const { noteId } = useParams();
   const api = import.meta.env.VITE_API;
-  const { isPending, isError, data, error } = useQuery({
-    queryKey: ["notes", noteId],
-    queryFn: async () => {
-      const response = await fetch(`${api}/note/${noteId}`);
-      const data = await response.json();
-      return data;
-    },
-  });
-  const note: Note = data;
+
+  useEffect(() => {
+    async function getNote(noteId: string | undefined) {
+      try {
+        const response = await fetch(`${api}/note/${noteId}`);
+        const data = await response.json();
+        setTitle(data.title);
+        setContent(data.content);
+        setIsPending(false);
+      } catch (error) {
+        setIsError(true);
+        setErrorMessage(error.message);
+      }
+    }
+    getNote(noteId);
+  }, [noteId]);
+
+  // const { isPending, isError, data, error } = useQuery({
+  //   queryKey: ["notes", noteId],
+  //   queryFn: async () => {
+  //     const response = await fetch(`${api}/note/${noteId}`);
+  //     const data = await response.json();
+  //     return data;
+  //   },
+  // });
+  // const note: Note = data;
+  // if (note) {
+  //   setTitle(note.title);
+  //   setContent(note.content);
+  // }
 
   if (isPending) return <p>Loading...</p>;
-  if (isError) return <p>{error.message}</p>;
+  if (isError) return <p>{errorMessage}</p>;
 
-  return (
-    <section>
-      <h2>{note.title}</h2>
-      <div>{note.content}</div>
-    </section>
-  );
+  if (editmode === false)
+    return (
+      <section className="note-data">
+        <h2>{title}</h2>
+        <div>{content}</div>
+        <button onClick={() => setEditmode(true)}>Edit</button>
+      </section>
+    );
+
+  if (editmode)
+    return (
+      <section className="note-data">
+        <input
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <textarea
+          name=""
+          id=""
+          value={content}
+          onChange={(event) => setContent(event.target.value)}
+        ></textarea>
+        <button onClick={() => setEditmode(false)}>Done</button>
+      </section>
+    );
 }
